6086ea32446cd28d3b688844e8bb1b0595826071
Rishi Rawat <121020112+rishirawat04@users.noreply.github.com>
2024-10-15 17:39:30 +0530

    Update api.ts

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api/',
+  baseURL: 'https://taskmanager-8kbj.onrender.com/api/',
   withCredentials: true,
 });
 
